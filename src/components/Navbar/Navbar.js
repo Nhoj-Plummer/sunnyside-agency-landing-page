a5a0b3391c639e639587566f9d27bbfc6c541408
Nhoj <nhojplummer@yahoo.com>
2023-10-04 13:02:18 -0400

    Making progresss of navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,39 @@
 import React from "react"
+import "./Navbar.css"
+import {GlobalLogo, GlobalHamburger} from "../../assets/assetsglobal"
+
+const Menu = () => {
+  return (
+    <>
+      <p>
+        <a href="#About"></a>
+      </p>
+      <p>
+        <a href="#Services"></a>
+      </p>
+      <p>
+        <a href="#About"></a>
+      </p>
+      <p>
+        <a href="#About"></a>
+      </p>
+    </>
+  )
+}
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className="navbar-container">
+      <div className="navbar-image-container">
+        <img src={GlobalLogo} alt="logo" />
+      </div>
+      <div className="navbar-mobile-container">
+        <div className="navbar-mobile-toggle">
+          <img src={GlobalHamburger} alt="hamburger" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
