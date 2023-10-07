1b90be8f67d62fbcb392393b0031678dc1eb6106
Nhoj <nhojplummer@yahoo.com>
2023-10-07 12:46:03 -0400

    Starting for progress

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,24 +2,22 @@ import React from "react"
 import "./Navbar.css"
 import {GlobalLogo, GlobalHamburger} from "../../assets/assetsglobal"
 
-const Menu = () => {
-  return (
-    <>
-      <p>
-        <a href="#About"></a>
-      </p>
-      <p>
-        <a href="#Services"></a>
-      </p>
-      <p>
-        <a href="#About"></a>
-      </p>
-      <p>
-        <a href="#About"></a>
-      </p>
-    </>
-  )
-}
+const Menu = () => (
+  <>
+    <p>
+      <a href="#about">About</a>
+    </p>
+    <p>
+      <a href="#services">Services</a>
+    </p>
+    <p>
+      <a href="#careers">Careers</a>
+    </p>
+    <p>
+      <a href="#about"> About</a>
+    </p>
+  </>
+)
 
 const Navbar = () => {
   return (
@@ -27,6 +25,13 @@ const Navbar = () => {
       <div className="navbar-image-container">
         <img src={GlobalLogo} alt="logo" />
       </div>
+
+      {/* Desktop Navbar */}
+      <div className="navbar-desktop-container">
+        <Menu />
+      </div>
+
+      {/* Mobile Navbar */}
       <div className="navbar-mobile-container">
         <div className="navbar-mobile-toggle">
           <img src={GlobalHamburger} alt="hamburger" />
