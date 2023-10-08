6cac463a1a48ad9d3ed13f6b17311597fd7b8ecd
Nhoj <nhojplummer@yahoo.com>
2023-10-08 17:47:29 -0400

    Making progress

--- src/components/Graphic/Graphic.js
+++ src/components/Graphic/Graphic.js
@@ -1,10 +1,26 @@
 import React from "react"
 import "./Graphic.css"
+import {DesktopEgg} from "../../assets/desktop/assetsdesktop.js"
+import {MobileEgg} from "../../assets/mobile/assetsmobile"
 
 const Graphic = () => {
-  return <div className="graphic-container">
-    
-  </div>
+  return (
+    <div className="graphic-container">
+      <div className="graphic-container_image">
+        <picture>
+          <source media="(min-width: 541px)" srcSet={DesktopEgg} />
+          <img src={MobileEgg} alt="img" />
+        </picture>
+      </div>
+      <div className="graphic-container_text">
+        <h1>Transform your brand</h1>
+        <p>We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
+        <span>
+          <a href="#learnmore">Learn More</a>
+        </span>
+      </div>
+    </div>
+  )
 }
 
 export default Graphic
