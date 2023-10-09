0a0aab4ffe6ec18b2c4313ab778ddece0f3d6e98
Nhoj <nhojplummer@yahoo.com>
2023-10-08 20:21:01 -0400

    Body section builting

--- src/components/Graphic/Graphic.js
+++ src/components/Graphic/Graphic.js
@@ -1,20 +1,19 @@
 import React from "react"
 import "./Graphic.css"
-import {DesktopEgg} from "../../assets/desktop/assetsdesktop.js"
-import {MobileEgg} from "../../assets/mobile/assetsmobile"
 
-const Graphic = () => {
+
+const Graphic = ({desktopimg, mobileimg, imageStyle, title, text}) => {
   return (
     <div className="graphic-container">
-      <div className="graphic-container_image">
+      <div className="graphic-container_image" style={imageStyle}>
         <picture>
-          <source media="(min-width: 541px)" srcSet={DesktopEgg} />
-          <img src={MobileEgg} alt="img" />
+          <source media="(min-width: 541px)" srcSet={desktopimg} />
+          <img src={mobileimg} alt="img" />
         </picture>
       </div>
       <div className="graphic-container_text">
-        <h1>Transform your brand</h1>
-        <p>We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
+        <h1>{title}</h1>
+        <p>{text}</p>
         <span>
           <a href="#learnmore">Learn More</a>
         </span>
--- src/containers/Body/Body.js
+++ src/containers/Body/Body.js
@@ -1,11 +1,16 @@
 import React from "react"
 import "./Body.css"
 import {Graphic} from "../../components/componentsindex"
+import {DesktopEgg, DesktopStandOut} from "../../assets/desktop/assetsdesktop.js"
+import {MobileEgg, MobileStandOut} from "../../assets/mobile/assetsmobile"
 
 const Body = () => {
+  const graphicStyle1 = {background: "var(--color-y)", order: "2"}
+  const graphicStyle2 = {background: "var(--color-sr)"}
   return (
     <div className="body-container">
-      <Graphic />
+      <Graphic desktopimg={DesktopEgg} mobileimg={MobileEgg} title={"Transform your brand"} imageStyle={graphicStyle1} text={"We are a full-service creative agency specializing in helping brands grow fast. ENgage your clients throuhg compelling visuals that do most of the marketing for you."} />
+      <Graphic desktopimg={DesktopStandOut} mobileimg={MobileStandOut} title={"Stand out to the right audience"} imageStyle={graphicStyle2} text={"Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we'll build and extend your branc in digital places."} />
     </div>
   )
 }
