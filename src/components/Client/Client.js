028afdcadd95ec106a3c9542db006bcc342b4c88
Nhoj <nhojplummer@yahoo.com>
2023-10-12 20:01:43 -0400

    making progress

--- src/components/Client/Client.js
+++ src/components/Client/Client.js
@@ -1,8 +1,20 @@
 import React from "react"
 import "./Client.css"
+import {GlobalEmily} from "../../assets/assetsglobal"
 
 const Client = () => {
-  return <div className="client-container">Client</div>
+  return (
+    <div className="client-container">
+      <div className="client-container_image">
+        <img src={GlobalEmily} alt="img" />
+      </div>
+      <p>We put our trust in Sunnyside and they delivered, making sure our needs were met and dealines were always hit</p>
+      <h2>Emily R.</h2>
+      <span>
+        <p>Marketing Director</p>
+      </span>
+    </div>
+  )
 }
 
 export default Client
