c808d9b7f0bf2698662a728c8902ddb66147a7b6
Nhoj <nhojplummer@yahoo.com>
2023-10-13 09:44:17 -0400

    Making progress

--- src/components/Client/Client.js
+++ src/components/Client/Client.js
@@ -1,17 +1,17 @@
 import React from "react"
 import "./Client.css"
-import {GlobalEmily} from "../../assets/assetsglobal"
 
-const Client = () => {
+
+const Client = ({personImg, text, name, job}) => {
   return (
     <div className="client-container">
       <div className="client-container_image">
-        <img src={GlobalEmily} alt="img" />
+        <img src={personImg} alt="img" />
       </div>
       <div className="client-container_text">
-        <p>We put our trust in Sunnyside and they delivered, making sure our needs were met and dealines were always hit</p>
-        <h2>Emily R.</h2>
-        <h4>Marketing Director</h4>
+        <p>{text}</p>
+        <h2>{name}</h2>
+        <h4>{job}</h4>
       </div>
     </div>
   )
--- src/containers/Reviews/Reviews.js
+++ src/containers/Reviews/Reviews.js
@@ -1,14 +1,15 @@
 import React from "react"
 import {Client} from "../../components/componentsindex"
 import "./Reviews.css"
+import {GlobalEmily, GlobalThomas, GlobalJennie} from "../../assets/assetsglobal"
 
 function Reviews() {
   return (
     <div className="reviews-container">
-      <h1>Client Testimonials</h1>
-      <Client />
-      <Client />
-      <Client />
+      <h1>CLIENT TESTIMONIALS</h1>
+      <Client personImg={GlobalEmily} text={"We put our trust in Sunnyside and they delivered, making sure our needs were met and ddeadlines were always hit."} name={" Emily R."} job={"Marketing Director"} />
+      <Client personImg={GlobalThomas} text={"Sunnyside's enthusiasm coupled with their keen interest in our brand's success made it a satisfying and enjoyable experience."} name={"Thomas S."} job={"Chief Operating Officer"} />
+      <Client personImg={GlobalJennie} text={"Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!"} name={"Jennie F."} job={"Business Owner"} />
     </div>
   )
 }
