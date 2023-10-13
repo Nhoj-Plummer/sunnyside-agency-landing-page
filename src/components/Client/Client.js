f6a6a758fa492a6bb796ae60c4beafd7e14256ea
Nhoj <nhojplummer@yahoo.com>
2023-10-12 20:36:46 -0400

    Making progress

--- src/components/Client/Client.js
+++ src/components/Client/Client.js
@@ -8,11 +8,11 @@ const Client = () => {
       <div className="client-container_image">
         <img src={GlobalEmily} alt="img" />
       </div>
-      <p>We put our trust in Sunnyside and they delivered, making sure our needs were met and dealines were always hit</p>
-      <h2>Emily R.</h2>
-      <span>
-        <p>Marketing Director</p>
-      </span>
+      <div className="client-container_text">
+        <p>We put our trust in Sunnyside and they delivered, making sure our needs were met and dealines were always hit</p>
+        <h2>Emily R.</h2>
+        <h4>Marketing Director</h4>
+      </div>
     </div>
   )
 }
